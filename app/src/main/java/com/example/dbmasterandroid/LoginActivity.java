1169f261dff48f595480c9e5d84a75d44076a4a9
TaeHyeongKim <uuzaza@naver.com>
2020-04-24 15:29:33 +0900

    #DBMA-9
    레이아웃 컴포넌트 바인딩

--- app/src/main/java/com/example/dbmasterandroid/LoginActivity.java
+++ app/src/main/java/com/example/dbmasterandroid/LoginActivity.java
@@ -3,12 +3,39 @@ package com.example.dbmasterandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private EditText ed_serverURL, ed_id, ed_pw;
+    private Button btn_login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        bindUI();
+    }
+
+    private void bindUI(){
+        ed_serverURL = findViewById(R.id.ed_serverURL);
+        ed_id = findViewById(R.id.ed_id);
+        ed_pw = findViewById(R.id.ed_pw);
+
+        btn_login = findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_login:
+                Toast.makeText(getApplicationContext(),"Login Clicked",Toast.LENGTH_SHORT).show();
+                break;
+        }
+
     }
 }
