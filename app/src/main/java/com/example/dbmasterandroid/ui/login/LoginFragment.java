0fec3742a676e054f97442f05b9ba7914fd10b45
최우영 <djdj7087@naver.com>
2020-04-28 23:22:35 +0900

    toolbar font 변경

--- app/src/main/java/com/example/dbmasterandroid/ui/login/LoginFragment.java
+++ app/src/main/java/com/example/dbmasterandroid/ui/login/LoginFragment.java
@@ -12,8 +12,6 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.NavAction;
-import androidx.navigation.NavHostController;
 import androidx.navigation.Navigation;
 
 import com.example.dbmasterandroid.R;
