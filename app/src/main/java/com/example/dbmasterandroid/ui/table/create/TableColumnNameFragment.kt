04f766223bfa387f0ae067db312da37eb94426b2
최우영 <djdj7087@naver.com>
2020-05-11 22:27:10 +0900

    #DBMA-93 칼럼 데이터타입 입력 UI 구현 중

--- app/src/main/java/com/example/dbmasterandroid/ui/table/create/TableColumnNameFragment.kt
+++ app/src/main/java/com/example/dbmasterandroid/ui/table/create/TableColumnNameFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import com.example.dbmasterandroid.R
 import kotlinx.android.synthetic.main.fragment_column_create_name.*
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
@@ -36,6 +37,7 @@ class TableColumnNameFragment: Fragment() {
 
         viewModel.columnNameValid.observe(viewLifecycleOwner, Observer {
             /* TODO 칼럼 데이터 타입 받는 화면으로 넘어가야 함. */
+            findNavController().navigate(R.id.action_tableColumnNameFragment_to_tableColumnTypeFragment)
         })
     }
 }
--- app/src/main/java/com/example/dbmasterandroid/ui/table/create/TableColumnTypeFragment.kt
+++ app/src/main/java/com/example/dbmasterandroid/ui/table/create/TableColumnTypeFragment.kt
@@ -1,7 +1,20 @@
 package com.example.dbmasterandroid.ui.table.create
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.dbmasterandroid.R
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 class TableColumnTypeFragment: Fragment() {
 
+    private val viewModel: TableCreateViewModel by sharedViewModel()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+
+        return inflater.inflate(R.layout.fragment_column_create_type, container, false)
+    }
 }
