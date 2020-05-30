5ad79e7cb5cfca97015d2319c67bd9afadd41943
최우영 <djdj7087@naver.com>
2020-05-30 13:27:36 +0900

    #DBMA-190 메인화면 구성 완료

--- app/src/main/java/com/example/dbmasterandroid/ui/table/data/TableDataFragment.kt
+++ app/src/main/java/com/example/dbmasterandroid/ui/table/data/TableDataFragment.kt
@@ -1,4 +1,24 @@
 package com.example.dbmasterandroid.ui.table.data
 
-class TableDataFragment {
+import com.example.dbmasterandroid.base.BaseFragment
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class TableDataFragment : BaseFragment<TableDataViewModel>() {
+    override val layoutResourceId: Int
+        get() = TODO("Not yet implemented")
+
+    override val viewModel: TableDataViewModel by viewModel()
+
+    override fun initView() {
+        TODO("Not yet implemented")
+    }
+
+    override fun initData() {
+        TODO("Not yet implemented")
+    }
+
+    override fun initFinish() {
+        TODO("Not yet implemented")
+    }
+
 }
---
+++ app/src/main/java/com/example/dbmasterandroid/ui/table/data/TableDataViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.dbmasterandroid.ui.table.data
+
+import com.example.dbmasterandroid.base.BaseViewModel
+
+class TableDataViewModel: BaseViewModel() {
+
+}
