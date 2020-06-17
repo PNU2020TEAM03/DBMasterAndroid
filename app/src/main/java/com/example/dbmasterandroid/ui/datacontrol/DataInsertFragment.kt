51ac366f62b65e7549ed4142332fb1483319981c
최우영 <djdj7087@naver.com>
2020-06-17 19:21:47 +0900

    #DBMA-220 반응형 레이아웃 구성 완료.

---
+++ app/src/main/java/com/example/dbmasterandroid/ui/datacontrol/DataInsertFragment.kt
@@ -0,0 +1,4 @@
+package com.example.dbmasterandroid.ui.datacontrol
+
+class DataInsertFragment {
+}
--- app/src/main/java/com/example/dbmasterandroid/ui/table/control/TableControlFragment.kt
+++ app/src/main/java/com/example/dbmasterandroid/ui/table/control/TableControlFragment.kt
@@ -2,6 +2,7 @@ package com.example.dbmasterandroid.ui.table.control
 
 import com.example.dbmasterandroid.R
 import com.example.dbmasterandroid.base.BaseFragment
+import kotlinx.android.synthetic.main.fragment_table_control.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class TableControlFragment: BaseFragment<TableControlViewModel>() {
@@ -19,7 +20,26 @@ class TableControlFragment: BaseFragment<TableControlViewModel>() {
     }
 
     override fun initFinish() {
-        TODO("Not yet implemented")
+        /* insert */
+        btn_table_control_insert.setOnClickListener {
+
+        }
+        /* update */
+        btn_table_control_update.setOnClickListener {
+
+        }
+        /* delete */
+        btn_table_control_delete.setOnClickListener {
+
+        }
+        /* join */
+        btn_table_control_join.setOnClickListener {
+
+        }
+        /* export */
+        btn_table_control_export.setOnClickListener {
+
+        }
     }
 
 }
