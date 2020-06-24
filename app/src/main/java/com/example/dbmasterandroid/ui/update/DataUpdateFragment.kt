e35ffb5ba5e605e904017b48d4fd00a42ac97f6c
최우영 <djdj7087@naver.com>
2020-06-24 23:49:49 +0900

    #DBMA-282 업데이트 구현 중

---
+++ app/src/main/java/com/example/dbmasterandroid/ui/update/DataUpdateFragment.kt
@@ -0,0 +1,22 @@
+package com.example.dbmasterandroid.ui.update
+
+import com.example.dbmasterandroid.base.BaseFragment
+
+class DataUpdateFragment: BaseFragment<DataUpdateViewModel>() {
+    override val layoutResourceId: Int
+        get() = TODO("Not yet implemented")
+    override val viewModel: DataUpdateViewModel
+        get() = TODO("Not yet implemented")
+
+    override fun initView() {
+        TODO("Not yet implemented")
+    }
+
+    override fun initData() {
+        TODO("Not yet implemented")
+    }
+
+    override fun initFinish() {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/example/dbmasterandroid/ui/update/DataUpdateViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.dbmasterandroid.ui.update
+
+import com.example.dbmasterandroid.base.BaseViewModel
+
+class DataUpdateViewModel: BaseViewModel() {
+
+}
